3e9f941a3cfe6490fd08e6a935025d8a76d94030
Julia117 <julialysenko08@gmail.com>
2020-02-25 14:46:11 +0100

    fix plot, used only two years

---
+++ plot2.R
@@ -0,0 +1,16 @@
+NEI <- readRDS("./exdata_data_NEI_data/summarySCC_PM25.rds")
+bltmr <- subset(NEI, fips == "24510")
+
+sum0 <- sum(subset(bltmr, year == 1999)$Emissions)
+
+sum3 <- sum(subset(bltmr, year == 2008)$Emissions)
+rng <- range(sum3, sum0, na.rm = T)
+
+png(file = "plot2.png", bg="white")
+
+with(bltmr, plot(1999, sum0, xlim = c(1998, 2009), ylim = rng, pch = 20, xlab = "Year", 
+               ylab = "Total emission in Baltimore, tons"))
+
+with(bltmr, points(2008, sum3, xlim = c(1998, 2009), ylim = rng, pch = 20))
+lines(c(1999, 2008),c(sum0, sum3))
+dev.off()
